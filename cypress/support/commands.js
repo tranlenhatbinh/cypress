a54725d96b53524bca2334940f3d00c808017b57
Binh Tran <binh.tran@asnet.com.vn>
2018-11-09 09:56:53 +0700

    First init

---
+++ cypress/support/commands.js
@@ -0,0 +1,33 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('NavigateWorldRoamer', () => {
+  cy.viewport(1300, 700)
+  cy.visit('/')
+  // Wait for Policy popup displays
+  cy.wait(1000)
+  cy.get('span').contains('OK').parent().click({force: true})
+})
